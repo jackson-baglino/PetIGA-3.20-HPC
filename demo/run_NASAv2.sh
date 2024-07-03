6c41d345454d24506f209a4b2a42cb125e8e973d
Jackson Baglino <jbaglino@caltech.edu>
2024-07-03 09:53:40 -0700

    chore: Update input directory path in run_NASAv2.sh

--- demo/run_NASAv2.sh
+++ demo/run_NASAv2.sh
@@ -52,12 +52,12 @@ echo $SLURM_SUBMIT_DIR
 
 # Define variable names to be exported -----------------------------------------
   # File names
-input_dir="/home/jbaglino/PetIGA-3.20/demo/input/"
+input_dir="/home/jbaglino/PetIGA-3.20-HPC/demo/input/"
 # inputFile=$input_dir"grainReadFile-88_s1-10_s2-21.dat"
 # inputFile=$input_dir"grainReadFile-165_s1-10_s2-30.dat"
-# inputFile=$input_dir"grainReadFile-10_s1-10.dat"
-inputFile=$input_dir"grainReadFile-5_s1-10.dat"
-
+inputFile=$input_dir"grainReadFile-10_s1-10.dat"
+# inputFile=$input_dir"grainReadFile-5_s1-10.dat"
+# inputFile=$input_dir"grainReadFile-2.dat"
 
 # Define simulation parameters -------------------------------------------------
 # Define dimensions
@@ -67,17 +67,17 @@ dim=3
 dim=$(echo "$dim" | bc -l)
 
 # Domain sizes
+# Lx=488.4e-6                    # Domain size X -- 2 Grain
+# Ly=244.2e-6                    # Domain size Y -- 2 Grain
+# Lz=244.2e-6                    # Domain size Z -- 2 Grain
+
 # Lx=0.35e-03                   # Domain size X -- 5 Grain
 # Ly=0.35e-03                   # Domain size Y -- 5 Grain
 # Lz=2.202e-04                  # Domain size Z -- 5 Grain
 
 Lx=0.5e-03                    # Domain size X -- 10 Grain
 Ly=0.5e-03                    # Domain size Y -- 10 Grain
-Lz=1.101e-04                  # Domain size Z -- 10 Grain
-
-# Lx=0.5e-03                    # Domain size X -- 10 Grain
-# Ly=0.5e-03                    # Domain size Y -- 10 Grain
-# Lz=2.202e-04                  # Domain size Z -- 10 Grain
+Lz=2.312e-04                  # Domain size Z -- 10 Grain
 
 # Lx=2.0e-3                     # Domain size X -- 88 Grain
 # Ly=2.0e-3                     # Domain size Y -- 88 Grain
@@ -89,13 +89,17 @@ Lz=1.101e-04                  # Domain size Z -- 10 Grain
 
 
 # Number of elements
+# Nx=264                        # Number of elements in X -- 2 Grain
+# Ny=132                        # Number of elements in Y -- 2 Grain
+# Nz=132                        # Number of elements in Z -- 2 Grain
+
 # Nx=193                       # Number of elements in X -- 5 Grain
 # Ny=193                       # Number of elements in Y -- 5 Grain
 # Nz=122                        # Number of elements in Z -- 5 Grain
 
-Nx=275                        # Number of elements in X -- 10 Grain
-Ny=275                        # Number of elements in Y -- 10 Grain
-Nz=122                        # Number of elements in Z -- 10 Grain
+Nx=270                        # Number of elements in X -- 10 Grain
+Ny=270                        # Number of elements in Y -- 10 Grain
+Nz=125                        # Number of elements in Z -- 10 Grain
 
 # Nx=385
 # Ny=385
@@ -112,7 +116,7 @@ Nz=122                        # Number of elements in Z -- 10 Grain
 
 # Time parameters
 delt_t=1.0e-4                 # Time step
-t_final=48*60*60              # Final time
+t_final=2*24*60*60              # Final time
 n_out=100                     # Number of output files
 
 # Convert scientific notation to decimal using bc
